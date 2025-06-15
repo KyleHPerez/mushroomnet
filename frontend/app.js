dc2c1d84ecd105bdb58aa677bd65ac15a7cbbbca
Kyle H. Perez <k.p13@icloud.com>
2025-06-15 11:04:41 -0400

    Added remaining feature mapping to app.js
    app.js feature map is now complete with mapping for all 22 features and feature values.

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const featureMap = {
   cap_shape: {
-    label: "Cap Shape",
+    label: "cap-shape",
     options: {
       b: "Bell",
       c: "Conical",
@@ -13,7 +13,7 @@ const featureMap = {
     },
   },
   cap_surface: {
-    label: "Cap Surface",
+    label: "cap-surface",
     options: {
       f: "Fibrous",
       g: "Grooves",
@@ -22,7 +22,7 @@ const featureMap = {
     },
   },
   cap_color: {
-    label: "Cap Color",
+    label: "cap-color",
     options: {
       n: "Brown",
       b: "Buff",
@@ -36,7 +36,201 @@ const featureMap = {
       y: "Yellow",
     },
   },
-  // Add remaining 19 features with the same structure
+  bruises: {
+    label: "bruises",
+    options: {
+      t: "Bruises",
+      f: "None",
+    },
+  },
+  odor: {
+    label: "odor",
+    options: {
+      a: "Almond",
+      l: "Anise",
+      c: "Creosote",
+      y: "Fishy",
+      m: "Musty",
+      n: "None",
+      p: "Pungent",
+      s: "Spicy",
+    },
+  },
+  gill_attachment: {
+    label: "gill-attachment",
+    options: {
+      a: "Attached",
+      d: "Descending",
+      f: "Free",
+      n: "Notched",
+    },
+  },
+  gill_spacing: {
+    label: "gill-spacing",
+    options: {
+      c: "Close",
+      w: "Crowded",
+      d: "Distant",
+    },
+  },
+  gill_size: {
+    label: "gill-size",
+    options: {
+      b: "Broad",
+      n: "Narrow",
+    },
+  },
+  gill_color: {
+    label: "gill-color",
+    options: {
+      k: "Black",
+      n: "Brown",
+      b: "Buff",
+      h: "Chocolate",
+      g: "Gray",
+      r: "Green",
+      o: "Orange",
+      p: "Pink",
+      u: "Purple",
+      e: "Red",
+      w: "White",
+      y: "Yellow",
+    },
+  },
+  stalk_shape: {
+    label: "stalk-shape",
+    options: {
+      e: "Enlarging",
+      t: "Tapering",
+    },
+  },
+  stalk_root: {
+    label: "stalk-root",
+    options: {
+      b: "Bulbous",
+      c: "Club",
+      u: "Cup",
+      e: "Equal",
+      z: "Rhizomorphs",
+      r: "Rooted",
+    },
+  },
+  stalk_surface_above_ring: {
+    label: "stalk-surface-above-ring",
+    options: {
+      f: "Fibrous",
+      y: "Scaly",
+      k: "Silky",
+      s: "Smooth",
+    },
+  },
+  stalk_surface_below_ring: {
+    label: "stalk-surface-below-ring",
+    options: {
+      f: "Fibrous",
+      y: "Scaly",
+      k: "Silky",
+      s: "Smooth",
+    },
+  },
+  stalk_color_above_ring: {
+    label: "stalk-color-above-ring",
+    options: {
+      n: "Brown",
+      b: "Buff",
+      g: "Gray",
+      o: "Orange",
+      p: "Pink",
+      e: "Red",
+      w: "White",
+      y: "Yellow",
+    },
+  },
+  stalk_color_below_ring: {
+    label: "stalk-color-below-ring",
+    options: {
+      n: "Brown",
+      b: "Buff",
+      g: "Gray",
+      o: "Orange",
+      p: "Pink",
+      e: "Red",
+      w: "White",
+      y: "Yellow",
+    },
+  },
+  veil_type: {
+    label: "veil-type",
+    options: {
+      p: "Partial",
+      u: "Universal",
+    },
+  },
+  veil_color: {
+    label: "veil-color",
+    options: {
+      n: "Brown",
+      o: "Orange",
+      y: "Yellow",
+    },
+  },
+  ring_number: {
+    label: "ring-number",
+    options: {
+      n: "None",
+      o: "One",
+      t: "Two",
+    },
+  },
+  ring_type: {
+    label: "ring-type",
+    options: {
+      c: "Cobwebby",
+      e: "Evanescent",
+      f: "Flaring",
+      l: "Large",
+      n: "None",
+      p: "Pendant",
+      s: "Sheathing",
+      z: "Zone",
+    },
+  },
+  spore_print_color: {
+    label: "spore-print-color",
+    options: {
+      k: "Black",
+      n: "Brown",
+      b: "Buff",
+      h: "Chocolate",
+      r: "Green",
+      o: "Orange",
+      u: "Purple",
+      w: "White",
+      y: "Yellow",
+    },
+  },
+  population: {
+    label: "population",
+    options: {
+      a: "Abundant",
+      c: "Clustered",
+      n: "Numerous",
+      s: "Scattered",
+      v: "Several",
+      y: "Solitary",
+    },
+  },
+  habitat: {
+    label: "habitat",
+    options: {
+      g: "Grasses",
+      l: "Leaves",
+      p: "Paths",
+      u: "Urban",
+      w: "Waste",
+      d: "Woods",
+    },
+  },
 };
 
 const MushroomForm = () => {
